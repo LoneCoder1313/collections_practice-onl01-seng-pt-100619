7065d87f9bc28d6d7dae4a3638b050b890e3a4d8
Ricky dildy <rdildy92@gmail.com>
2019-11-01 03:43:35 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -28,5 +28,9 @@ def #sort_array_asc(numbers)
        end 
      end 
      
-      
-      
+      array = ["blake", "ashley", "scott"]
+      def #kesha_maker(array)
+        do kesha_maker(array).each |array|
+        #expect 3rd character of each element to be a dollar sign.
+        #expect (kesha_maker(["blake", "ashley", "scott").to eq(["bl$ke", "as$ley", "sc$tt"])
+        puts [""]
